749fe63e32f297e7791ea698d8da12065c4bd934
cloudnine-ddy <jiadon5474@gmail.com>
2026-04-09 21:59:49 +0800

    feat(navbar): create navbar profile card and logout logic

--- frontend/mern-notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/mern-notes-app/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import ProfileInfo from '../Cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
 
 const Navbar = () => {
+
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate('/login');
+  }
+
   return (
-    <div className="bg-white flx items-center justify-between px-6 py-2 drop-shadow">
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium tect-black py-2">Notes</h2>
+    
+      <ProfileInfo onLogout={onLogout} />
+    
     </div>
-  )
-}
+  );
+};
 
 export default Navbar

e9b4ec2be45a551689372134c8dfa761c968a80c
cloudnine-ddy <jiadon5474@gmail.com>
2026-04-10 00:35:13 +0800

    feat(card): create note card

--- frontend/mern-notes-app/src/pages/Home/Home.jsx
+++ frontend/mern-notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard 
+            title = "Meeting on 7th April" 
+            date = "3rd April 2024"c
+            content = "Meeting on 7th April Meeting on 7th April"
+            tags = "#Meeting"
+            isPinned = {true} 
+            onEdit = {() => {}} 
+            onDelete = {() => {}}
+            onPinNote = {() => {}}
+          />
+        </div>
+      </div>
+
     </>
   );
 };

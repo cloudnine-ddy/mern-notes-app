23c6808c8009d2895c616cd1325a44a8d9ed2f18
cloudnine-ddy <jiadon5474@gmail.com>
2026-04-12 02:10:19 +0800

    feat(api): get all notes  api integration

--- frontend/mern-notes-app/src/pages/Home/Home.jsx
+++ frontend/mern-notes-app/src/pages/Home/Home.jsx
@@ -14,6 +14,8 @@ const Home = () => {
     data: null,
   });
 
+  const [allNotes, setAllNotes] = useState([]);
+
   const [userInfo, setUserInfo] = useState(null);
 
   const navigate = useNavigate();
@@ -33,28 +35,43 @@ const Home = () => {
     }
   };
 
+  // Get all notes
+  const getAllNotes = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-notes");
+
+      if (response.data && response.data.notes) {
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log("An error occurred while fetching notes");
+    }
+  };
   useEffect(() => {
+    getAllNotes();
     getUserInfo();
     return () => {};
   }, []);
 
   return (
     <>
-      <Navbar userInfo={userInfo}/>
+      <Navbar userInfo={userInfo} />
 
       <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-4 mt-8">
-          <NoteCard
-            title="Meeting on 7th April"
-            date="3rd April 2024"
-            c
-            content="Meeting on 7th April Meeting on 7th April"
-            tags="#Meeting"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          />
+          {allNotes.map((item, index) => (
+            <NoteCard
+              key={item._id}
+              title={item.title}
+              date={item.createdOn}
+              content={item.content}
+              tags={item.tags}
+              isPinned={item.isPinned}
+              onEdit={() => {}}
+              onDelete={() => {}}
+              onPinNote={() => {}}
+            />
+          ))}
         </div>
       </div>
 
